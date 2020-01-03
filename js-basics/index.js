7f2a0cc283715ff5cbd3da4447c40c4a24280536
Dave Brown <db1967@talktalk.net>
2020-01-03 16:40:34 +0000

    Jan_3_2020

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,3 +1,12 @@
-let name = 'Dave';
-let age = 30;
-let isApproved = true;
+function checkDriverAge() {
+    var age = Number(prompt("What is your age?"));
+	if (age < 18) {
+		alert("Sorry, you are too young to drive this car. Powering off.");
+	} else if (age > 18) {
+		alert("Powering On. Enjoy the ride!");
+	} else if (age === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
+}
+
+checkDriverAge();
